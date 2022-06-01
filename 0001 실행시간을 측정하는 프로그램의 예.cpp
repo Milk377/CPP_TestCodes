b1a9af925e350594bf20781e5b1c2080a9ef9548
KIM TAE HOON <naxoruby@gmail.com>
2022-06-01 16:42:06 +0900

    Create 0001 실행시간을 측정하는 프로그램의 예.cpp
    #include <ctime> =  #include<time.h>
    #include <cstdio> = #include<stdio.h>
    
    자료형 clolck_t

---
+++ 0001 실행시간을 측정하는 프로그램의 예.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int codes1(int n) {
+	int result, sum;
+	sum = 0;
+	for (int i = 1; i < n; i++) {
+		sum += i;
+	}
+	result = sum;
+	return result;
+}
+
+int main(int argc, char* argsv[]) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	clock_t start, finish;
+	double duration;
+	start = clock();
+
+	//test codes
+	int sum = codes1(10000000);
+
+	finish = clock();
+	
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("%d", &sum);
+	printf("%f\n", duration);
+
+
+	
+	
+	
+
+
+}
