efe91e690bcc77115f768efa88c32d9ba1a1d42a
KIM TAE HOON <naxoruby@gmail.com>
2022-06-03 06:50:07 +0900

    Create 0002 n까지의 합을 3가지로 구현해서 시행시간 비교하기.cpp

---
+++ 0002 n까지의 합을 3가지로 구현해서 시행시간 비교하기.cpp
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int codes1(int n) {
+	int result, sum;
+	sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+	result = sum;
+	return result;
+}
+
+int codes2(int n) {
+	return n * (n + 1) / 2;
+}
+
+int codes3(int n) {
+	int sum = 0;
+	
+	for (int i = 1; i <= n; i++) {
+		
+		int add = 0;
+		for (int j = 0; j < i; j++) {
+			add += 1;
+		}
+
+		sum += add;
+	}
+
+	return sum;
+}
+
+int main(int argc, char* argsv[]) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	clock_t start, finish;
+	double duration;
+	start = clock();
+
+	//test codes
+	int n = 100000;
+	//cout << codes1(n) << "\n";
+	//cout << codes2(n) << "\n";
+	cout << codes3(n)<<"\n";
+
+	finish = clock();
+	
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("%f\n", duration);
+
+}
